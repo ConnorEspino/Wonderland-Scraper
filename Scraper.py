f7f8da6fdf276ff7603b7073bcabb4a90eda4107
Connor Espino <connorespino@u.boisestate.edu>
2021-11-23 12:37:05 -0700

    Fixed Balance variable

--- Scraper.py
+++ Scraper.py
@@ -5,9 +5,10 @@ session = HTMLSession()
 url = 'https://snowtrace.io/token/0x136Acd46C134E8269052c62A67042D6bDeDde3C9?a=0xE840E73aE7087D2A582B8fE56caC95b624a71D82'
 
 r = session.get(url)
-container = r.html.find('div.col-md.u-ver-divider.u-ver-divider--left.u-ver-divider--none-md', first = True)
+container = r.html.find('div.col-md.u-ver-divider.u-ver-divider--left.u-ver-divider--none-md', first = True).text
 
-balance = container.search('{} MEMO')[0]
+#Remove unnecessary strings and cast as float
+balance = container.replace("Balance\n", "")
+balance = float(balance.replace(" MEMO", ""))
 
-balance = balance.replace(" ", "")
 print(balance)
