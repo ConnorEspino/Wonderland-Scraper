6854fbf10ba39a893a36280b4d24e67f299ff170
Connor Espino <connorespino@u.boisestate.edu>
2021-11-23 13:49:46 -0700

    Correct format, ROI not implemented #1

--- Scraper.py
+++ Scraper.py
@@ -1,15 +1,39 @@
 from requests_html import HTMLSession
+import csv
+import time
 
-#Read from URL file later
-session = HTMLSession()
-url = 'https://snowtrace.io/token/0xfe8eb55d570e679b02b56e81ea9cba2847d75324?a=0x689f45d3ff4f72f49248b7b0188c3b39e182d0c7'
+def __init__():
+    #TODO: Error handling for improper URL/Public keys
+    #TODO: Allow option to output to google docs? If possible
+    file = open("pubkey.txt", "r")
 
-r = session.get(url)
-container = r.html.find('div.col-md.u-ver-divider.u-ver-divider--left.u-ver-divider--none-md', first = True).text
+    #Read the public key from the first line of the pubkey file
+    url = 'https://snowtrace.io/token/0x136acd46c134e8269052c62a67042d6bdedde3c9?a=' + file.readline()
 
-#Remove unnecessary strings and cast as float
-balance = container.replace("Balance\n", "")
-balance = balance.replace(",", "")
-balance = (float) (balance.replace(" MEMO", ""))
+    file.close()
 
-print(balance)
+    session = HTMLSession()
+
+    #Open the webpage and search for the right place to find balance
+    r = session.get(url)
+    container = r.html.find('div.col-md.u-ver-divider.u-ver-divider--left.u-ver-divider--none-md', first = True).text
+
+    #Remove unnecessary strings and cast as float
+    balance = container.replace("Balance\n", "")
+    balance = balance.replace(",", "")
+    balance = (float) (balance.replace(" MEMO", ""))
+
+    #Get current price of TIME
+    r = session.get('https://coinmarketcap.com/currencies/wonderland/')
+    priceString = r.html.find('div.priceValue')[0].text
+
+    #Create a float representation of current price
+    currPrice = priceString.replace("$", "")
+    currPrice = (float) (currPrice.replace(",", ""))
+
+
+    with open('output.csv', 'w') as file:
+        writer = csv.writer(file, delimiter = ",")
+        writer.writerow([time.ctime(time.time()),str(priceString),str(round(balance, 5)),str(round(currPrice*balance, 2)),"ROI Here"])
+
+__init__()
