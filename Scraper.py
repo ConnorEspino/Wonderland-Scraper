f19f6bf9be2bff80075bd731038fedbc8a6f26ab
Connor Espino <connorespino@u.boisestate.edu>
2021-11-23 12:14:37 -0700

    Complete functionality for getting balance

---
+++ Scraper.py
@@ -0,0 +1,13 @@
+from requests_html import HTMLSession
+
+#Read from URL file later
+session = HTMLSession()
+url = 'https://snowtrace.io/token/0x136Acd46C134E8269052c62A67042D6bDeDde3C9?a=0xE840E73aE7087D2A582B8fE56caC95b624a71D82'
+
+r = session.get(url)
+container = r.html.find('div.col-md.u-ver-divider.u-ver-divider--left.u-ver-divider--none-md', first = True)
+
+balance = container.search('{} MEMO')[0]
+
+balance = balance.replace(" ", "")
+print(balance)
