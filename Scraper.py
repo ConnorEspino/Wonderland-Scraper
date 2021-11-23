933bdb33699d6949aeeec1b14b99b815ba162008
Connor Espino <connorespino@u.boisestate.edu>
2021-11-23 12:51:23 -0700

    Fixed large number string formatting

--- Scraper.py
+++ Scraper.py
@@ -2,13 +2,14 @@ from requests_html import HTMLSession
 
 #Read from URL file later
 session = HTMLSession()
-url = 'https://snowtrace.io/token/0x136Acd46C134E8269052c62A67042D6bDeDde3C9?a=0xE840E73aE7087D2A582B8fE56caC95b624a71D82'
+url = 'https://snowtrace.io/token/0xfe8eb55d570e679b02b56e81ea9cba2847d75324?a=0x689f45d3ff4f72f49248b7b0188c3b39e182d0c7'
 
 r = session.get(url)
 container = r.html.find('div.col-md.u-ver-divider.u-ver-divider--left.u-ver-divider--none-md', first = True).text
 
 #Remove unnecessary strings and cast as float
 balance = container.replace("Balance\n", "")
-balance = float(balance.replace(" MEMO", ""))
+balance = balance.replace(",", "")
+balance = (float) (balance.replace(" MEMO", ""))
 
 print(balance)
